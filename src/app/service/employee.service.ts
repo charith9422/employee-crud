1d7de1ce4e17a005ab3e47d86b413a54b77baeae
Charith Abeygunawardhana <charith9422@gmail.com>
2020-05-10 20:09:43 +0530

    Error handlers added

--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -3,6 +3,8 @@ import * as configuration from '../../assets/configuration.json';
 import { Observable } from 'rxjs';
 import { Employee } from '../model/employee';
 import { HttpClient, HttpResponse } from '@angular/common/http';
+import { catchError, retry } from 'rxjs/operators';
+import { HttpErrorService, HandleError } from './http-error.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,30 +12,49 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
 export class EmployeeService {
 
   private empIdToEdit;
+  private handleError: HandleError;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private httpErrorHandler: HttpErrorService) {
+    this.handleError = httpErrorHandler.createHandleError('EmployeeService');
+  }
 
   host = configuration.host;
   employees = configuration.endpoints.employees;
 
   getEmployees(): Observable<Employee[]> {
-    return this.http.get<Employee[]>(`${this.host}${this.employees}`);
+    return this.http.get<Employee[]>(`${this.host}${this.employees}`)
+      .pipe(
+        retry(3),
+        catchError(this.handleError('getEmployees', []))
+      );
   }
 
   addEmployee(employee: Employee): Observable<Employee> {
-    return this.http.post<Employee>(`${this.host}${this.employees}`, employee);
+    return this.http.post<Employee>(`${this.host}${this.employees}`, employee)
+      .pipe(
+        catchError(this.handleError('addEmployee', employee))
+      );
   }
 
   deleteEmployee(id: number): Observable<{}> {
-    return this.http.delete(`${this.host}${this.employees}/${id}`);
+    return this.http.delete(`${this.host}${this.employees}/${id}`)
+      .pipe(
+        catchError(this.handleError('deleteEmployee'))
+      );
   }
 
   updateEmployee(employee: Employee): Observable<Employee> {
-    return this.http.put<Employee>((`${this.host}${this.employees}/${employee.id}`), employee);
+    return this.http.put<Employee>((`${this.host}${this.employees}/${employee.id}`), employee)
+      .pipe(
+        catchError(this.handleError('updateEmployee', employee))
+      );
   }
 
   getEmployeeById(id: number): Observable<Employee> {
-    return this.http.get<Employee>(`${this.host}${this.employees}/${id}`);
+    return this.http.get<Employee>(`${this.host}${this.employees}/${id}`)
+      .pipe(
+        retry(3)
+      );
   }
 
   get empId() {
